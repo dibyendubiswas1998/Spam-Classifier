8bed65fb4068cdaefbca82e91a4cffe4c4005d57
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2023-06-25 15:59:50 +0530

    initial setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="v.0.0.1",
+    author="dibyendubiswas1998",
+    author_email="dibyendubiswas1998@gmail.com",
+    description="Web App for Spam-Classifier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dibyendubiswas1998/Spam-Classifier.git",
+    packages=["src"],
+    license="GNU",
+    python_requires=">=3.10",
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        'nltk'
+    ]
+)
